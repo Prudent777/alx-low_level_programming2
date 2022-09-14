60c17568bd1ff6b6d8fcc1d63cb06612e6202d37
Prudent777 <prudentngaipera4@gmail.com>
2022-09-14 04:35:10 -0700

    0 Task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,36 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"                                                                                                                     
 
-#include <studio>
+                                                                                                                                      
 
-void positive_ornegative(int i);
-int largest_number(int a,int b,int c);
-void print_remaining_days(int month,int day,int year);
-int convert_day(int month,int day);
-void print_times_table(int n);
+/**                                                                                                                                   
 
-#endif
+  * main - test function for positif or  negative                                                                                     
+
+  *return: 0                                                                                                                          
+
+  */                                                                                                                                  
+
+                                                                                                                                      
+
+  int main(void)                                                                                                                      
+
+  {                                                                                                                                   
+
+  int i;                                                                                                                              
+
+                                                                                                                                      
+
+  i = 0;                                                                                                                              
+
+  positf-or_negative(i);                                                                                                              
+
+  return (0);                                                                                                                         
+
+  }            
+^3
+^C
+clear
+
+
+
+
